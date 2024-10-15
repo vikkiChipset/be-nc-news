3560215d40379a83e78d83c82a05813a1141d18e
vikkiChipset <vikkitorriya@gmail.com>
2024-10-15 11:21:23 +0100

    Add error-handling.js for customer errors and complete task 4

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -16,7 +16,6 @@ describe("GET /api", () => {
       .expect(200)
       .then(({ body }) => {
         expect(body).toEqual(endpoints);
-        console.log(endpoints);
       });
   });
 });
@@ -35,6 +34,7 @@ describe("GET /api/topics", () => {
       });
   });
 });
+
 describe("GET /api/nonexistent", () => {
   test("responds with 404 if passed incorrect name", () => {
     return request(app)
@@ -42,6 +42,43 @@ describe("GET /api/nonexistent", () => {
       .expect(404)
       .then(({ body }) => {
         expect(body.msg).toBe("Endpoint Does Not Exist");
+      });
+  });
+});
+
+describe("GET /api/articles/:article_id", () => {
+  test("responds with 200 and an article object with the key 'article'", () => {
+    return request(app)
+      .get("/api/articles/1")
+      .expect(200)
+      .then(({ body }) => {
+        expect(typeof body.article).toBe("object");
+        expect(body.article.author).toBe("butter_bridge");
+        expect(body.article.title).toBe("Living in the shadow of a great man");
+        expect(body.article.article_id).toBe(1);
+        expect(body.article.body).toBe("I find this existence challenging");
+        expect(body.article.topic).toBe("mitch");
+        expect(body.article.created_at).toBe("2020-07-09T20:11:00.000Z");
+        expect(body.article.votes).toBe(100);
+        expect(body.article.article_img_url).toBe(
+          "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700"
+        );
+      });
+  });
+  test("responds 400 with an error message for an invalid article type", () => {
+    return request(app)
+      .get("/api/articles/not-a-number")
+      .expect(400)
+      .then(({ body }) => {
+        expect(body.msg).toBe("Invalid data type");
+      });
+  });
+  test("responds 404 with an error message when given an article_id is valid, but does not exist", () => {
+    return request(app)
+      .get("/api/articles/123")
+      .expect(404)
+      .then(({ body }) => {
+        expect(body.msg).toBe("Not Found");
       });
   });
 });
---
+++ controllers/articles.controller.js
@@ -0,0 +1,17 @@
+const { fetchArticleById } = require("../models/articles.model");
+
+exports.getArticleById = (req, res, next) => {
+  const { article_id } = req.params;
+
+  if (isNaN(article_id)) {
+    return res.status(400).send({ msg: "Invalid data type" });
+  }
+
+  fetchArticleById(article_id)
+    .then((article) => {
+      res.status(200).send({ article });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
