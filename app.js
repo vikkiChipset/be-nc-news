c75c0e548b40aa6744eeb2089dddd235197e4dd0
vikkiChipset <vikkitorriya@gmail.com>
2024-10-15 15:36:58 +0100

    add /api/articles endpoint with sorting and error handling, and test it

--- app.js
+++ app.js
@@ -3,14 +3,19 @@ const app = express();
 const { getTopics } = require("./controllers/topics.controller");
 const { getApi } = require("./controllers/endpoints.controller");
 const { getArticleById } = require("./controllers/articles.controller");
-const { psqlErrorHandler, customErrorHandler, serverErrorHandler } = require("./error-handling");
+const {
+  psqlErrorHandler,
+  customErrorHandler,
+  serverErrorHandler,
+} = require("./error-handling");
+const { getArticles } = require("./controllers/articles.controller");
 
 app.use(express.json());
 
 app.get("/api/topics", getTopics);
 app.get("/api", getApi);
 app.get("/api/articles/:article_id", getArticleById);
-
+app.get("/api/articles", getArticles);
 
 app.all("*", (req, res) => {
   res.status(404).send({ msg: "Endpoint Does Not Exist" });
--- controllers/articles.controller.js
+++ controllers/articles.controller.js
@@ -1,15 +1,22 @@
 const { fetchArticleById } = require("../models/articles.model");
+const { fetchAllArticles } = require("../models/articles.model");
 
 exports.getArticleById = (req, res, next) => {
   const { article_id } = req.params;
 
-  if (isNaN(article_id)) {
-    return res.status(400).send({ msg: "Invalid data type" });
-  }
-
   fetchArticleById(article_id)
     .then((article) => {
       res.status(200).send({ article });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
+
+exports.getArticles = (req, res, next) => {
+  fetchAllArticles()
+    .then((articles) => {
+      res.status(200).send({ articles });
     })
     .catch((err) => {
       next(err);
