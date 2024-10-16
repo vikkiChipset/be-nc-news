2ea892524529e04156d12d175dc3813dba4509c5
vikkiChipset <vikkitorriya@gmail.com>
2024-10-16 12:53:09 +0100

    Implement POST /api/articles/:article_id/comments endpoint with error handling and tests

--- controllers/comments.controller.js
+++ controllers/comments.controller.js
@@ -1,4 +1,7 @@
-const { fetchCommentsByArticleId } = require("../models/comments.model");
+const {
+  fetchCommentsByArticleId,
+  insertComment,
+} = require("../models/comments.model");
 
 exports.getCommentsByArticleId = (req, res, next) => {
   const { article_id } = req.params;
@@ -6,6 +9,18 @@ exports.getCommentsByArticleId = (req, res, next) => {
   fetchCommentsByArticleId(article_id)
     .then((comments) => {
       res.status(200).send({ comments });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
+
+exports.postCommentByArticleID = (req, res, next) => {
+  const { article_id } = req.params;
+  const { username, body } = req.body;
+  insertComment(article_id, username, body)
+    .then((comment) => {
+      res.status(201).send({ comment });
     })
     .catch((err) => {
       next(err);
